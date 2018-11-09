40a983883727129afe724eb9ae0dcb162415af5e
kariby <43680637+kariby@users.noreply.github.com>
2018-11-09 20:03:33 +0500

    Add files via upload

---
+++ SRV2/SRV1/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace SRV1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.lbBuf = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbWork = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(34, 315);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(230, 315);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 5;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // lbBuf
+            // 
+            this.lbBuf.FormattingEnabled = true;
+            this.lbBuf.Location = new System.Drawing.Point(34, 41);
+            this.lbBuf.Name = "lbBuf";
+            this.lbBuf.Size = new System.Drawing.Size(149, 251);
+            this.lbBuf.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Producer";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(227, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Consumer";
+            // 
+            // lbWork
+            // 
+            this.lbWork.FormattingEnabled = true;
+            this.lbWork.Location = new System.Drawing.Point(230, 41);
+            this.lbWork.Name = "lbWork";
+            this.lbWork.Size = new System.Drawing.Size(222, 251);
+            this.lbWork.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(488, 350);
+            this.Controls.Add(this.lbWork);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbBuf);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Lab2";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.ListBox lbBuf;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lbWork;
+    }
+}
+
---
+++ SRV3/SRVLab3/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace SRVLab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbFromImage = new System.Windows.Forms.ListBox();
+            this.tbToConsole = new System.Windows.Forms.TextBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.tbCommand = new System.Windows.Forms.TextBox();
+            this.lblCommand = new System.Windows.Forms.Label();
+            this.lblTextToConsole = new System.Windows.Forms.Label();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbFromImage
+            // 
+            this.lbFromImage.FormattingEnabled = true;
+            this.lbFromImage.Location = new System.Drawing.Point(12, 98);
+            this.lbFromImage.Name = "lbFromImage";
+            this.lbFromImage.Size = new System.Drawing.Size(479, 225);
+            this.lbFromImage.TabIndex = 0;
+            // 
+            // tbToConsole
+            // 
+            this.tbToConsole.Location = new System.Drawing.Point(261, 29);
+            this.tbToConsole.Name = "tbToConsole";
+            this.tbToConsole.Size = new System.Drawing.Size(230, 20);
+            this.tbToConsole.TabIndex = 1;
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(12, 55);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(91, 24);
+            this.btnRun.TabIndex = 2;
+            this.btnRun.Text = "Start";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnPush_Click);
+            // 
+            // tbCommand
+            // 
+            this.tbCommand.Location = new System.Drawing.Point(12, 29);
+            this.tbCommand.Name = "tbCommand";
+            this.tbCommand.Size = new System.Drawing.Size(230, 20);
+            this.tbCommand.TabIndex = 3;
+            // 
+            // lblCommand
+            // 
+            this.lblCommand.AutoSize = true;
+            this.lblCommand.Location = new System.Drawing.Point(9, 8);
+            this.lblCommand.Name = "lblCommand";
+            this.lblCommand.Size = new System.Drawing.Size(59, 13);
+            this.lblCommand.TabIndex = 4;
+            this.lblCommand.Text = "Application";
+            // 
+            // lblTextToConsole
+            // 
+            this.lblTextToConsole.AutoSize = true;
+            this.lblTextToConsole.Location = new System.Drawing.Point(258, 9);
+            this.lblTextToConsole.Name = "lblTextToConsole";
+            this.lblTextToConsole.Size = new System.Drawing.Size(54, 13);
+            this.lblTextToConsole.TabIndex = 5;
+            this.lblTextToConsole.Text = "Command";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(261, 55);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(79, 25);
+            this.btnSend.TabIndex = 6;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(212, 328);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(79, 25);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Stop";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(508, 365);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.lblTextToConsole);
+            this.Controls.Add(this.lblCommand);
+            this.Controls.Add(this.tbCommand);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.tbToConsole);
+            this.Controls.Add(this.lbFromImage);
+            this.Name = "Form1";
+            this.Text = "Lab3";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbFromImage;
+        private System.Windows.Forms.TextBox tbToConsole;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.TextBox tbCommand;
+        private System.Windows.Forms.Label lblCommand;
+        private System.Windows.Forms.Label lblTextToConsole;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
